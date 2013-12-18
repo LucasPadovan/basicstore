15f52a4b335c3ca542b795de8a666b3923633fd2
LucasPadovan <zerobba8@hotmail.com>
2013-12-18 18:11:59 -0300

    compras animadas con el carrito

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -1,13 +1,16 @@
 var toggleFloatingCart = function(){
-    var cart = $('[data-element="floating-cart"]')
+    var
+        cart = $('[data-element="floating-cart"]')
+        , $button = $('[data-action="toggle-cart"]')
 
     if($(cart).css('left') == '-10px') {
         changeCartTooltip('Abrir el carrito')
         closeCart(cart)
+        $button.tooltip('hide')
     }
     else {
         changeCartTooltip('Cerrar el carrito')
-        $('[data-action="toggle-cart"]').tooltip('show')
+        $button.tooltip('show')
         openCart(cart)
     }
 }
@@ -26,13 +29,38 @@ var changeCartTooltip = function(new_title){
 
 var changeTargetForButton = function(target){
     var
-        element = '#add_' + target
-        , new_target = 'line_' + target
-    $(element).attr('data-replace', true)
-    $(element).attr('data-target', new_target)
-    $(element).attr('data-respond-as', 'put')
+        element = '#add_product_' + target
+        , new_id = 'add_product_' + target
+        , new_target = 'line_product_' + target
+        , new_button = '<a href="/es/line_items?product_id=' + target + '" class="btn btn-warning add-to-cart-fill" data-callback="updateCart" data-error="' + new_target + '" data-method="post" data-remote="true" data-replace="true" data-respond-as="put" data-target="' + new_target + '" id="' + new_id + '" rel="nofollow">Añadir al Carrito</a>'
+    $(element).replaceWith(new_button)
+}
+
+var updateCart = function(){
+    updateCartTotal()
+    animatePurchase()
 }
 
 var updateCartTotal = function(){
+    var
+        total = 0
+        , elements = $('[data-type="price"]')
+        , cart_total = $('[data-type="cart-total"]')[0]
+    $.each(elements, function( index, value ) {
+        total += parseFloat(value.innerHTML)
+    })
+    cart_total.innerHTML = total
+}
+
+var animatePurchase = function(){
+    toggleFloatingCart()
+    highlightItem()
+    setTimeout(function(){
+        toggleFloatingCart()
+    }, 2000)
+}
+
+var highlightItem = function(){
 
 }
+

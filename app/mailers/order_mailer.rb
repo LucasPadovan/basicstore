f358e49c67d8b90e99c920a8b81158a5521a26b0
LucasPadovan <zerobba8@hotmail.com>
2013-12-28 19:17:19 -0300

    layout para el email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,33 +1,35 @@
 class OrderMailer < ActionMailer::Base
   default from: 'Lucas Padovan <lucaslockon@gmail.com>'
+  layout 'mail'
 
+  #todo: definir cual de estos se queda
   def received(order)
     @order = order
     
-    mail to: order.email, subject: 'Confirmacion de pedido'
+    mail to: order.email, subject: 'Confirmacion de pedido', content_type: 'text/html'
   end
 
   def shipped(order)
     @order = order
 
-    mail to: order.email, subject: 'Orden embarcada'
+    mail to: order.email, subject: 'Orden embarcada', content_type: 'text/html'
   end
 
   def ready(order)
     @order = order
 
-    mail to: order.email, subject: 'Orden lista'
+    mail to: order.email, subject: 'Orden lista', content_type: 'text/html'
   end
 
   def delayed(order)
     @order = order
 
-    mail to: order.email, subject: 'Orden demorada'
+    mail to: order.email, subject: 'Orden demorada', content_type: 'text/html'
   end
 
   def cancelled(order)
     @order = order
 
-    mail to: order.email, subject: 'Orden cancelada'
+    mail to: order.email, subject: 'Orden cancelada', content_type: 'text/html'
   end
 end

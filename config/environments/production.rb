e6f5348026f4fff1de8796cbb62f07f3b971bc60
LucasPadovan <zerobba8@hotmail.com>
2013-12-30 19:18:38 -0300

    ahora esta la url en el email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Tienda::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'unmundodeovejitas.com.ar'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.live.com",

731fe05da6618759dcaef50916b2dde732942d3d
LucasPadovan <zerobba8@hotmail.com>
2014-01-07 20:01:56 -0300

    muchos fix de tamaños y colores

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 
-RAINBOW_COLORS = %w(red orange yellow green light_blue blue)
+RAINBOW_COLORS = %w(red orange yellow green light_blue cyan blue violet)
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
 
 module Tienda

3ada4fd5b5b092f1ca5b7b80a83a62f279cc54d5
LucasPadovan <zerobba8@hotmail.com>
2014-01-15 21:08:52 -0300

    varios fixes menores

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,6 +1,5 @@
 module NavHelper
   def store_urls(product_type)
-    #todo: cuando este en store_path sin ningun parametro deberia marcar las novedades como pestaña porque es la sección a la que se accede
     urls = []
     urls << store_index_path(product_type: product_type.name)
     urls

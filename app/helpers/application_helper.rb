4c3922a2164b62cebe2dcbca36dd223587908043
LucasPadovan <zerobba8@hotmail.com>
2014-01-20 00:00:00 -0300

    srbuj helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,4 +49,56 @@ module ApplicationHelper
 
     link_to content_tag(:i, '', class: 'icon-remove'), *args, options
   end
+
+  def srbuj_link_to_new(name, *args)
+    options = args.extract_options!
+
+    options['class'] ||= 'btn btn-primary stitched'
+    options['title'] ||= t('label.new')
+    options['data-show-tooltip'] ||= true
+    options['data-remote'] = true
+    options['data-target'] ||= 'modal'
+    options['data-modal'] ||= true
+    link_to content_tag(:i, '', class: 'icon-plus-sign') + ' ' + name, *args, options
+  end
+
+  def srbuj_link_to_edit(*args)
+    options = args.extract_options!
+
+    options['class'] ||= 'iconic'
+    options['title'] ||= t('label.edit')
+    options['data-show-tooltip'] ||= true
+    options['data-remote'] = true
+    options['data-target'] ||= 'modal'
+    options['data-modal'] ||= true
+
+    link_to content_tag(:i, '', class: 'icon-pencil'), *args, options
+  end
+
+  def srbuj_link_to_show(*args)
+    options = args.extract_options!
+
+    options['class'] ||= 'iconic'
+    options['title'] ||= t('label.show')
+    options['data-show-tooltip'] ||= true
+    options['data-remote'] = true
+    options['data-target'] ||= 'modal'
+    options['data-modal'] ||= true
+
+    link_to content_tag(:i, '', class: 'icon-zoom-in'), *args, options
+  end
+
+  def srbuj_link_to_destroy(*args)
+    options = args.extract_options!
+
+    options['class'] ||= 'iconic'
+    options['title'] ||= t('label.delete')
+    options['method'] ||= :delete
+    options['data-confirm'] ||= t('messages.confirmation')
+    options['data-show-tooltip'] ||= true
+    options['data-remote'] = true
+    options['data-target'] ||= nil
+
+    link_to content_tag(:i, '', class: 'icon-remove'), *args, options
+  end
 end
